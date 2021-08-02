5a31d14c37d4ae55eb8a3d091231ce2fcf51eb8b
Wang-Songxiang <1374806247@qq.com>
2021-08-03 01:23:26 +0900

    v1.0完成，可自选购买次数，未打包

--- potionbuyer.py
+++ potionbuyer.py
@@ -6,36 +6,69 @@ import xlrd
 import time
 import sys
 
-n  = sys.argv[1]
+n  = int(sys.argv[1])
 
 titlename = "PrincessConnectReDive"
 hwnd = win32gui.FindWindow(0, titlename)
 left, top, right, bottom = win32gui.GetWindowRect(hwnd)
 shell = win32com.client.Dispatch("WScript.Shell")
 shell.SendKeys('%')
+win32gui.BringWindowToTop(hwnd)
 win32gui.SetForegroundWindow(hwnd)
-win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
+# win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
 print(left)
 print(top)
 print(right)
 print(bottom)
-
+L=right-left
+W=bottom-top
+print(L)
+print(W)
+print(int(L*(518/1280)))
+print(int(W*(240/760)))
+time.sleep(0.1)
 i=0
 for i in range(n):
-    win32api.SetCursorPos((left+520,top+235))
+    time.sleep(1.5)
+    win32api.SetCursorPos((left+int(L*(518/1280)),top+int(W*(240/760))))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    win32api.SetCursorPos((left+750,top+235))
+    time.sleep(0.1)
+    win32api.SetCursorPos((left+int(L*(750/1280)),top+int(W*(240/760))))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    win32api.SetCursorPos((left+972,top+235))
+    time.sleep(0.1)
+    win32api.SetCursorPos((left+int(L*(972/1280)),top+int(W*(240/760))))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    win32api.SetCursorPos((left+1200,top+235))
+    time.sleep(0.1)
+    win32api.SetCursorPos((left+int(L*(1200/1280)),top+int(W*(240/760))))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
 
-    win32api.SetCursorPos((left+520,bottom-170))
+    time.sleep(0.1)
+    win32api.SetCursorPos((left+int(L*(518/1280)),bottom-int(W*(175/760))))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    win32api.SetCursorPos((left+750,bottom-170))
+    time.sleep(0.1)
+    win32api.SetCursorPos((left+int(L*(750/1280)),bottom-int(W*(175/760))))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    win32api.SetCursorPos((left+972,bottom-170))
+    time.sleep(0.1)
+    win32api.SetCursorPos((left+int(L*(972/1280)),bottom-int(W*(175/760))))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    win32api.SetCursorPos((left+1200,bottom-170))
+    time.sleep(0.1)
+    win32api.SetCursorPos((left+int(L*(1200/1280)),bottom-int(W*(175/760))))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+
+    time.sleep(0.1)
+    win32api.SetCursorPos((right-int(L*(220/1280)),bottom-int(W*(135/760))))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+
+    time.sleep(0.5)
+    win32api.SetCursorPos((right-int(L*(500/1280)),bottom-int(W*(80/760))))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    time.sleep(2)
+    win32api.SetCursorPos((right-int(L*(650/1280)),bottom-int(W*(80/760))))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    if i<n-1:
+        time.sleep(1)
+        win32api.SetCursorPos((right-int(L*(500/1280)),bottom-int(W*(140/760))))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(0.5)
+        win32api.SetCursorPos((right-int(L*(500/1280)),bottom-int(W*(230/760))))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
