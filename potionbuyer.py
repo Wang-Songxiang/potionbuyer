d3d8ac442cd94dc2c7994c02046cda329b288e4d
Wang-Songxiang <1374806247@qq.com>
2021-08-12 14:56:31 +0900

    2021/8/12更新

--- potionbuyer.py
+++ potionbuyer.py
@@ -5,8 +5,15 @@ import win32com.client
 import time
 import sys
 
-with open("times.txt", "r") as f:
-    n = int(f.read())
+with open("config.txt", "r", encoding="utf-8") as f:
+    n = int(f.readline().strip("购买次数:"))
+    print(n)
+    potion=f.readline()
+    if potion[5]=="是":
+        potion=1
+    stone=f.readline()
+    if stone[6]=="是":
+        stone=1
 
 titlename = "PrincessConnectReDive"
 hwnd = win32gui.FindWindow(0, titlename)
@@ -29,31 +36,33 @@ print(int(W*(240/760)))
 time.sleep(0.1)
 i=0
 for i in range(n):
-    time.sleep(1.5)
-    win32api.SetCursorPos((left+int(L*(518/1280)),top+int(W*(240/760))))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    time.sleep(0.1)
-    win32api.SetCursorPos((left+int(L*(750/1280)),top+int(W*(240/760))))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    time.sleep(0.1)
-    win32api.SetCursorPos((left+int(L*(972/1280)),top+int(W*(240/760))))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    time.sleep(0.1)
-    win32api.SetCursorPos((left+int(L*(1200/1280)),top+int(W*(240/760))))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    time.sleep(1)
+    if potion==1:
+        win32api.SetCursorPos((left+int(L*(518/1280)),top+int(W*(240/760))))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(0.1)
+        win32api.SetCursorPos((left+int(L*(750/1280)),top+int(W*(240/760))))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(0.1)
+        win32api.SetCursorPos((left+int(L*(972/1280)),top+int(W*(240/760))))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(0.1)
+        win32api.SetCursorPos((left+int(L*(1200/1280)),top+int(W*(240/760))))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
 
-    time.sleep(0.1)
-    win32api.SetCursorPos((left+int(L*(518/1280)),bottom-int(W*(175/760))))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    time.sleep(0.1)
-    win32api.SetCursorPos((left+int(L*(750/1280)),bottom-int(W*(175/760))))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    time.sleep(0.1)
-    win32api.SetCursorPos((left+int(L*(972/1280)),bottom-int(W*(175/760))))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    time.sleep(0.1)
-    win32api.SetCursorPos((left+int(L*(1200/1280)),bottom-int(W*(175/760))))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    if stone==1:
+        time.sleep(0.1)
+        win32api.SetCursorPos((left+int(L*(518/1280)),bottom-int(W*(175/760))))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(0.1)
+        win32api.SetCursorPos((left+int(L*(750/1280)),bottom-int(W*(175/760))))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(0.1)
+        win32api.SetCursorPos((left+int(L*(972/1280)),bottom-int(W*(175/760))))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(0.1)
+        win32api.SetCursorPos((left+int(L*(1200/1280)),bottom-int(W*(175/760))))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
 
     time.sleep(0.1)
     win32api.SetCursorPos((right-int(L*(220/1280)),bottom-int(W*(135/760))))
@@ -62,7 +71,7 @@ for i in range(n):
     time.sleep(0.5)
     win32api.SetCursorPos((right-int(L*(500/1280)),bottom-int(W*(80/760))))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    time.sleep(2)
+    time.sleep(1)
     win32api.SetCursorPos((right-int(L*(650/1280)),bottom-int(W*(80/760))))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
     if i<n-1:
